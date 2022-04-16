6d3132e267f1c045db65d43e80b741da7472978a
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2022-04-16 15:43:37 -0500

    Building out navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,19 @@ import { Link } from 'react-router-dom'
 
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
   return (
     <>
         <nav className="navbar">
             <div className='navbar=container'>
+                {/* <img src='kourierLogo.png' height="200" width="200" /> */}
                 <Link to="/" className="navbar-logo">
-                    
+                    Kourier
                 </Link>
+
+                
             </div>
         </nav>
     </>
